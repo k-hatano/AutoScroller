7bc6f9d9399dcc4b586c9b62b51ea0bb7a6c8104
kenta.hatano <kenta.hatano@s-cubism.jp>
2017-05-29 11:03:17 +0900

    固定されたタブのすぐ右に移動させたら固定、固定されていないタブのすぐ左に移動させたら固定解除

--- background.js
+++ background.js
@@ -7,10 +7,30 @@ chrome.tabs.onAttached.addListener( function(tabId, info){
 );
 
 chrome.tabs.onMoved.addListener( function(tabId, info){
-	if (info.toIndex == 0) {
-		chrome.tabs.get(tabId, function(tab){
-			chrome.tabs.update(tabId, { 'pinned':true });
-		});
-	}
+	chrome.tabs.get(tabId, function(tab){
+		if (tab.pinned) {
+			chrome.windows.get(info.windowId, {'populate':true}, function(aWindow){
+				if (aWindow.tabs.length - 1 == info.toIndex) {
+					chrome.tabs.update(tabId, { 'pinned':false });
+				} else {
+					chrome.windows.get(info.windowId, {'populate':true}, function(aWindow){
+						if (!aWindow.tabs[info.toIndex + 1].pinned) {
+							chrome.tabs.update(tabId, { 'pinned':false });
+						}
+					});
+				}
+			});
+		} else {
+			if (info.toIndex == 0) {
+				chrome.tabs.update(tabId, { 'pinned':true });
+			} else {
+				chrome.windows.get(info.windowId, {'populate':true}, function(aWindow){
+					if (aWindow.tabs[info.toIndex - 1].pinned) {
+						chrome.tabs.update(tabId, { 'pinned':true });
+					}
+				});
+			}
+		}
+	});
 }
 );
--- content.js
+++ content.js
@@ -13,3 +13,4 @@ function heartbeat(){
 }
 
 heartbeat();
+
