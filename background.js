80baab3084dc5e829890ee4adb7dc53bcd9bbe24
kenta.hatano <kenta.hatano@s-cubism.jp>
2017-06-07 11:02:17 +0900

    選択→非選択を繰り返したら固定

--- background.js
+++ background.js
@@ -23,18 +23,28 @@ chrome.tabs.onHighlightChanged.addListener( function(tabId, info){
 	console.log("onHighlightChanged");
 });
 
+var lastHighlighted = [];
+var lastHighlightedTime = 0;
+
 chrome.tabs.onHighlighted.addListener( function(info){
 	console.log("onHighlighted");
-	chrome.tabs.getAllInWindow(info.windowId, function(tabs){
-		if (info.tabIds.length == tabs.length && tabs.length > 1) {
-			chrome.tabs.get(info.tabIds[0], function(tab){
-				var pinned = tab.pinned;
-				for (var i = 0; i < info.tabIds.length; i++) {
-					chrome.tabs.update(info.tabIds[i], { 'pinned':!pinned });
-				}
-			});
+	console.log(info.tabIds);
+	var date = new Date();
+	var tmpLastHighlighted = lastHighlighted;
+
+	if (date.getTime() - lastHighlightedTime < 1000 && tmpLastHighlighted.length > info.tabIds.length) {
+		for (var i = 0; i < tmpLastHighlighted.length; i++) {
+			if (info.tabIds.indexOf(tmpLastHighlighted[i]) < 0) {
+				chrome.tabs.get(tmpLastHighlighted[i], function(tab){
+					var pinned = tab.pinned;
+					chrome.tabs.update(tab.id, { 'pinned':!pinned });
+				});
+			}
 		}
-	});
+	}
+
+	lastHighlighted = info.tabIds;
+	lastHighlightedTime = date.getTime();
 });
 
 chrome.tabs.onDetached.addListener( function(tabId, info){
@@ -63,6 +73,12 @@ chrome.tabs.onAttached.addListener( function(tabId, info){
 
 chrome.tabs.onMoved.addListener( function(tabId, info){
 	console.log("onMoved");
+
+	var date = new Date();
+	if (date.getTime() - lastHighlightedTime < 1000 ) {
+		return;
+	}
+
 	chrome.tabs.get(tabId, function(tab){
 		if (tab.pinned) {
 			chrome.windows.get(info.windowId, {'populate':true}, function(aWindow){
